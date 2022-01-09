7cd4d5c13154ff2be505a62c7b24ae87550facd2
Andrei Domkin <Andrei_Domkin@epam.com>
2022-01-09 22:10:38 +0300

    feat: add application restart on file changes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import app from './app';
 import ENV from './common/config';
 
 try {
-  app.listen(ENV.PORT as string, '0.0.0.0', () => process.stdout.write(`App is running on http://localhost:${ENV.PORT}\n`));
+  app.listen(ENV.PORT as string, '0.0.0.0', () =>
+    process.stdout.write(`App is running on http://localhost:${ENV.PORT}\n`)
+  );
 } catch (error) {
   process.stderr.write('Something went wrong\n');
   app.log.error(error);
