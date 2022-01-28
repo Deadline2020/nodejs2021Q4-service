15e38cc5bf4b9f5abd58ed5166abfbb8b68c183a
Andrei Domkin <Andrei_Domkin@epam.com>
2022-01-28 22:33:05 +0300

    feat: add docker settings

--- nest-js/.eslintrc.js
+++ nest-js/.eslintrc.js
@@ -1,19 +1,40 @@
 module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    jasmine: true,
+    node: true,
+    jest: true,
+  },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    ecmaVersion: 2021,
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
+  },
+  settings: {
+    noInlineConfig: true,
+    import/resolver: {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+    node: {
+      resolvePath: ['__dirname'],
+      tryExtensions: ['.js', '.ts'],
+    },
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    "eslint:recommended",
     'plugin:@typescript-eslint/recommended',
+    "plugin:node/recommended",
     'plugin:prettier/recommended',
+    "airbnb-base",
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
